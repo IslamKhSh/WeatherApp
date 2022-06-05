4a62555d0ea2e9405412dabdc114ede25328dd5a
Islam Khaled <islam.khaled50@gmail.com>
2022-06-06 01:04:59 +0200

    refactor: remove unused libs, classes, methods and resources

--- core/src/main/java/com/musala/weatherApp/core/extensions/SnackbarExt.kt
+++
@@ -1,8 +0,0 @@
-package com.musala.weatherApp.core.extensions
-
-import android.view.View
-import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
-import com.google.android.material.snackbar.Snackbar
-
-fun View.showSnackbar(message: String, length: Int = LENGTH_SHORT) =
-    Snackbar.make(this, message, length).show()
